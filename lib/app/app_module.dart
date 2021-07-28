8254d2e1988b334d280a5677b3acbe64b5667307
Kelveng <kelveng.magalhaes@mjv.com.br>
2021-07-27 22:02:34 -0300

    refatorar bind Dio

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,5 +1,5 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
-import 'package:estaciona_mais/app/features/home/external/api/client/custom_dio.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'common/network/network_info.dart';
@@ -8,7 +8,8 @@ import 'features/home/home_module.dart';
 class AppModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind((i) => CustomDio()),
+    Bind((i) => Dio(BaseOptions(
+        baseUrl: 'https://estacionamaisbackend.herokuapp.com/api'))),
     Bind((i) => NetworkInfoImpl(DataConnectionChecker())),
   ];
 
